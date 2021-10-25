210328f198d12f1ebc93bf1d2eee33257aab8689
Rick <ricksmycodeguy@gmail.com>
2021-10-25 12:26:51 -0500

    task 1 complete and passing

--- U5-M2-Homework/Task1.py
+++ U5-M2-Homework/Task1.py
@@ -1,2 +1,38 @@
 import math
-print(help(dir(math)))
+# print("---------math--------")
+# print(help(dir(math)))
+
+"""
+find helper function
+def function to iterate over lists
+use fucntion to discover index position of bob
+return idex
+"""
+# print("---------Contains--------")
+# print(dir("contains"))
+
+# print("-------Index----------")
+# print(dir("index"))
+
+# print("--------index.find---------")
+# print(dir("index".find))
+
+# print("--------index.find---------")
+# print(help("index".find))
+
+def csWhereIsBob(names):
+    str1 = ' '.join(names)
+    # print(str1)
+    strIndex_position = str1.find("Bob")
+    if strIndex_position >= 0:
+        NamesIndex_position = names.index("Bob")
+        return(NamesIndex_position)
+    else: return(-1)
+""" 
+note YOU MUST USE PRINT instead of RETURN to see the answers first. Then replace "print" with "returnr" when submitting to codesignal. 
+"""    
+csWhereIsBob(["Jimmy", "Layla", "Bob"]) 
+csWhereIsBob(["Bob", "Layla", "Kaitlyn", "Patricia"])
+csWhereIsBob(["Jimmy", "Layla", "James"])
+csWhereIsBob(["Bob", "Nathan", "Hayden"])
+csWhereIsBob(["Jimmy", "Layla", "Mandy"])
---
+++ U5-M2-Homework/Task2.py
@@ -0,0 +1,13 @@
+"""
+- define alphabet list [a, ..., z]
+- create empty list
+- slice given groups as length
+- iterate over alphabet list with a while loop using years as iterations
+- append list with each additional pair
+- convert list to string
+
+1 <= years <= 10
+1 <= groups <=26
+"""
+
+def csSchoolYearsAndGroups(years, groups):
