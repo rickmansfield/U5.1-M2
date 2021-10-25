6d3481afad5c68e3b1ad6b1b40a0b72f7a89209b
Rick <ricksmycodeguy@gmail.com>
2021-10-25 16:09:33 -0500

    Task 2 complete and passing

--- U5-M2-Homework/Task2.py
+++ U5-M2-Homework/Task2.py
@@ -9,5 +9,22 @@
 1 <= years <= 10
 1 <= groups <=26
 """
-
+from typing import Mapping
+# print(dir(Mapping))
 def csSchoolYearsAndGroups(years, groups):
+    alphaGroups = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+    slicedGroups = alphaGroups[:groups]
+    print(slicedGroups)
+    finalList = []
+    count = 0
+ 
+    while count < years:
+        count += 1
+        # print(count)
+        for i in slicedGroups:
+            countToString = str(count)
+            # print(countToString) 
+            finalList.append(countToString+i)
+    return(', '.join(finalList))
+
+csSchoolYearsAndGroups(4, 7)
