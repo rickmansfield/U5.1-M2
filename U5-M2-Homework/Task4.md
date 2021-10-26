b757304abcfd61337217643d65c3237f426ac383
Rick <ricksmycodeguy@gmail.com>
2021-10-25 19:02:06 -0500

    Test 4 passing

---
+++ U5-M2-Homework/Task4.md
@@ -0,0 +1,14 @@
+
+# Codewriting
+
+## Instructions
+- Given a string of words, return the length of the shortest word(s).
+
+## Notes:
+
+The input string will never be empty and you do not need to validate for different data types.
+[execution time limit] 4 seconds (py3)
+
+[input] string input_str
+
+[output] integer
---
+++ U5-M2-Homework/Task4.py
@@ -0,0 +1,32 @@
+"""
+convert string of words into individual strings in a list
+see researched dir for min(), and len()
+get min() length of each string using len() as key i.e. key=len
+return smallest string
+
+NOTE
+def length(s):
+    l = map(len, s.split())
+    return max(l), min(l)
+
+NOTE 
+s = "some strings"
+l = s.split()
+max(l, key=len)
+min(l, key=len)
+"""
+print(dir(min))
+print(help(min))
+print(dir(len))
+print(help(len))
+
+str1 = "This test finds the shortest sting of words or strings of words"
+str2 = "Always code as if the guy who ends up maintaining your code will be a violent psychopath who knows where you live"
+def csShortestWord(input_str):
+    stringToList = input_str.split()
+    min(stringToList, key=len)
+    return len(min(stringToList, key=len))
+
+
+print(csShortestWord(str1))
+print(csShortestWord(str2))
